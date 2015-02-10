bd7d29d504b5f1f48d04c8fa28333856eed34642
Patrick Kaeding <pkaeding@launchdarkly.com>
2015-02-09 17:22:10 -0800

    disabled ssl peer verification to allow bypassing fastly

--- src/LaunchDarkly/EventProcessor.php
+++ src/LaunchDarkly/EventProcessor.php
@@ -85,7 +85,9 @@ class EventProcessor {
   
     try {
 
-      $socket = @pfsockopen($protocol . "://" . $this->_host, $this->_port, $errno, $errstr, $this->_timeout);
+      $context = stream_context_create();
+      $result = stream_context_set_option($context, 'ssl', 'verify_peer', false); 
+      $socket = @pfsockopen($protocol . "://" . $this->_host, $this->_port, $errno, $errstr, $this->_timeout, $context);
 
       if ($errno != 0) {
         error_log("LaunchDarkly error opening socket $errno");
